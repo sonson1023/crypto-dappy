a4c14e84f4105ea16fcc4651ee903fcd36cd7d4e
seongwan-inno <78513970+seongwan-inno@users.noreply.github.com>
2022-04-19 18:41:24 +0900

    mission-02

--- src/hooks/use-dappy-templates.hook.js
+++ src/hooks/use-dappy-templates.hook.js
@@ -1,26 +1,38 @@
-import { useEffect, useReducer } from 'react'
-import { defaultReducer } from '../reducer/defaultReducer'
-import DappyClass from '../utils/DappyClass'
-import { generateDappies } from '../utils/dappies.utils'
+import { useEffect, useReducer } from "react";
+import { defaultReducer } from "../reducer/defaultReducer";
+import DappyClass from "../utils/DappyClass";
+// import { generateDappies } from "../utils/dappies.utils";
+
+import { query } from "@onflow/fcl";
+import { LIST_DAPPY_TEMPLATES } from "../flow/list-dappy-templates.script";
 
 export default function useDappyTemplates() {
-  const [state, dispatch] = useReducer(defaultReducer, { loading: false, error: false, data: [] })
+  const [state, dispatch] = useReducer(defaultReducer, {
+    loading: false,
+    error: false,
+    data: [],
+  });
 
   useEffect(() => {
     const fetchDappyTemplates = async () => {
-      dispatch({ type: 'PROCESSING' })
+      dispatch({ type: "PROCESSING" });
       try {
-        let res = generateDappies()
-        let mappedDappies = Object.values(res).map(d => {
-          return new DappyClass(d?.templateID, d?.dna, d?.name, d?.price)
-        })
-        dispatch({ type: 'SUCCESS', payload: mappedDappies })
+        let res = await query({
+          cadence: LIST_DAPPY_TEMPLATES,
+        });
+        console.log("res is", res);
+        // let res = generateDappies();
+        let mappedDappies = Object.values(res).map((d) => {
+          return new DappyClass(d?.templateID, d?.dna, d?.name, d?.price);
+        });
+        dispatch({ type: "SUCCESS", payload: mappedDappies });
       } catch (err) {
-        dispatch({ type: 'ERROR' })
+        console.log("err is ", err);
+        dispatch({ type: "ERROR" });
       }
-    }
-    fetchDappyTemplates()
-  }, [])
+    };
+    fetchDappyTemplates();
+  }, []);
 
-  return state
+  return state;
 }
